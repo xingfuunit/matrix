d26a8c7943c80fc3dd57aca603f8956630228cf7
pangh <jason@pzfresh.com>
2015-04-14 19:33:34 +0800

    接口类key和value处理过程修改

--- application/libraries/apiv/ec_to_erp/store_trade_add.php
+++ application/libraries/apiv/ec_to_erp/store_trade_add.php
@@ -20,7 +20,7 @@ class Store_trade_add {
     	$response_data['consignee'] = json_encode(
     												array('r_time'=>$request_data['receiver_time'],
     												'addr'=>$request_data['receiver_address'],
-    												'zip'=>$request_data['receiver_address'],
+    												'zip'=>$request_data['receiver_zip'],
     												'mobile'=>$request_data['receiver_mobile'],
     												'telephone'=>$request_data['receiver_phone'],
     												'area_city'=>$request_data['receiver_city'],
@@ -30,7 +30,7 @@ class Store_trade_add {
     												'name'=>$request_data['receiver_name'],	
     		));
     	
-    //	$response_data['app_id'] = $request_data['app_id'];
+    	$response_data['app_id'] = 'ecos.ome';
     //	$response_data['sign'] = $request_data['sign'];
     	$response_data['currency'] = $request_data['currency'];
     	$response_data['node_type'] = $request_data['node_type'];
@@ -40,15 +40,15 @@ class Store_trade_add {
     	$replace = array('pmt_describe','pmt_amount',);
     	$response_data['pmt_detail'] = str_replace($find,$replace,$request_data['promotion_details']);
     	$response_data['custom_mark'] = '';//未知字段
-    	$response_data['cost_tax'] = $request_data['pay_cost'];
+    	$response_data['cost_tax'] = '0.00';//订单税率
     	$response_data['lastmodify'] = $request_data['lastmodify'];
     	$response_data['branch_id'] = $request_data['branch_id'];
-    //	$response_data['consigner'] = array();//发货人
+    	$response_data['consigner'] = array();//发货人
     	$response_data['title'] = $request_data['title'];
     	$response_data['tax_type'] = $request_data['tax_type'];
     	$response_data['node_version'] = '2.0';
     	$response_data['from_type'] = 'ecos.b2c';
-    	$response_data['payinfo'] = json_encode(array('pay_name'=>$request_data['payment_type'],'cost_payment'=>$request_data['payed_fee']));
+    	$response_data['payinfo'] = json_encode(array('pay_name'=>$request_data['payment_type'],'cost_payment'=>$request_data['pay_cost']));
     	$response_data['branch_name_user'] = $request_data['branch_name_user'];
     	$response_data['order_bn'] = $request_data['tid'];
     	$response_data['tax_content'] = $request_data['tax_content'];
@@ -57,12 +57,12 @@ class Store_trade_add {
     	$response_data['pay_status'] = get_pay_status($request_data['pay_status']);
     //	var_dump($request_data['status']);exit; 
     	$response_data['status'] = get_order_status($request_data['status']);
-    	$response_data['pmt_goods'] = $request_data['discount_fee'];
-    	$response_data['score_u'] = '0.00';//未知字段
+    	$response_data['pmt_goods'] = $request_data['goods_discount_fee'];//商品促销优惠
+    	$response_data['score_u'] = $request_data['point_fee'];//使用积分支付的金额
     	$response_data['pmt_order'] = $request_data['orders_discount_fee'];
-    	$response_data['timestamp'] = time();
-    	$response_data['member_info'] = json_encode(array('tel'=>$request_data['buyer_mobile'],'addr'=>$request_data['buyer_address'],'mobile'=>$request_data['buyer_mobile'],'uname'=>$request_data['buyer_uname'],'name'=>$request_data['buyer_name']));
-    	$response_data['from_api_v'] = '2.0';
+    	$response_data['timestamp'] = time();//不确定
+    	$response_data['member_info'] = json_encode(array('tel'=>$request_data['buyer_phone'],'addr'=>$request_data['buyer_address'],'mobile'=>$request_data['buyer_mobile'],'uname'=>$request_data['buyer_uname'],'name'=>$request_data['buyer_name']));
+    	$response_data['from_api_v'] = $request_data['from_api_v'];
     	$response_data['task'] = $request_data['task'];
     	$response_data['discount'] = $request_data['discount_fee'];
     	$response_data['receiver_community'] = $request_data['receiver_community'];
@@ -83,65 +83,80 @@ class Store_trade_add {
     	$response_data['is_auto_complete'] = $request_data['is_auto_complete'];
     	$response_data['node_id'] = $request_data['from_node_id'];
     //	$response_data['order_objects'] = $request_data['from_node_id'];
-    	$response_data['payments'] = '[]';//未知字段
+    
+    	$request_order = json_decode($request_data['orders']);    	
+    	$order_objects = array();
+    	if ($request_order) {
+    		foreach ($request_order->order as $key=>$value) {
+    			$tmp = new stdClass;
+    			$tmp->consign_time = $value->consign_time;
+    			$tmp->obj_type = $value->type;
+    			$tmp->name = $value->title;
+    			$tmp->weight = $value->weight;
+    			$tmp->pmt_price = $value->discount_fee;//未确定字段
+    			$tmp->bn = $value->orders_bn;
+    			$tmp->oid = $value->oid;
+    			$tmp->amount = $value->total_order_fee;
+    			$tmp->score = '';//订单积分？
+    			$tmp->shop_goods_id = $value->iid;//未确定字段
+    			$tmp->obj_alias = $value->type_alias;
+    			$tmp->order_status = $value->order_status;
+    			$tmp->price = $value->price;
+    			$tmp->quantity = $value->items_num;
+    			$order_items = array();
+    			foreach ($value->order_items->item as $key2=>$value2) {
+    				$items = new stdClass;
+    				$items->score = $value2->score;
+    				$items->addon = '';//未确定字段
+    				$items->name = $value2->name;
+    				$items->weight = $value2->weight;
+    				$items->pmt_price = $value2->discount_fee;
+    				$items->bn = $value2->bn;
+    				$items->item_status = $value2->item_status;
+    				$sku_properties = explode(':',$value2->sku_properties);
+    				$pattr = new stdClass;
+    				$pattr->value = $sku_properties[1];
+    				$pattr->label = $sku_properties[0];
+    				$items->product_attr = array(0=>$pattr);
+    				$items->item_type = $value2->item_type;
+    				$items->amount = $value2->total_item_fee;
+    				$items->shop_goods_id = $value2->iid;//未确定字段
+    				$items->amount = $value2->total_item_fee;
+    				$items->sendnum = $value2->sendnum;
+    				$items->sale_price = $value2->sale_price;
+    				$items->quantity = $value2->num;
+    				$items->price = $value2->price;
+    				$items->shop_product_id = $value2->sku_id;
+    				$order_items[] = $items;
+    			}
+    			$tmp->order_items = $order_items;
+    			$order_objects[] = $tmp;
+    		}
+    	}   	 
+    	$response_data['order_objects'] = json_encode($order_objects);
+    	
+    	$request_payments = json_decode($request_data['payment_lists']);
+    	$payment_objects = array();
+    	if($request_payments){
+    		foreach ($request_payments->payment_list as $k=>$v) {
+    			$std = new stdClass;
+    			$std->trade_no = $v->payment_id;
+    			$std->account = $v->seller_account;
+    			$std->paymethod = $v->payment_name;
+    			$std->money = $v->pay_fee;
+    			$std->memo = $v->memo;
+    			$std->paycost = $v->paycost;
+    			$std->pay_account = $v->buyer_account;
+    			$std->bank = $v->seller_bank;
+    			$payment_objects[] = $std;
+    		}
+    	}
+    	$response_data['payments'] = json_encode($payment_objects);
+    	
     	$response_data['is_tax'] = 0;//未知字段
     	$response_data['createtime'] = strtotime($request_data['modified']);
     	$response_data['buyer_id'] = $request_data['buyer_id'];
-    	
-		$request_order = json_decode($request_data['orders']);
-		
-		$order_objects = array();
-		if ($request_order) {
-			foreach ($request_order->order as $key=>$value) {
-				$tmp = new stdClass;
-				$tmp->consign_time = $value->consign_time;
-				$tmp->obj_type = $value->type;
-				$tmp->name = $value->title;
-				$tmp->weight = $value->weight;
-				$tmp->pmt_price = $value->discount_fee;//未确定字段
-				$tmp->bn = $value->orders_bn;
-				$tmp->oid = $value->oid;
-				$tmp->amount = $value->total_order_fee;
-				$tmp->score = '';//订单积分？
-				$tmp->shop_goods_id = $value->iid;//未确定字段
-				$tmp->obj_alias = $value->type_alias;
-				$tmp->order_status = $value->order_status;
-				$tmp->price = $value->price;
-				$tmp->quantity = $value->items_num;
-				$order_items = array();
-				foreach ($value->order_items->item as $key2=>$value2) {
-					$items = new stdClass;
-					$items->score = $value2->score;
-					$items->addon = '';//未确定字段
-					$items->name = $value2->name;
-					$items->weight = $value2->weight;
-					$items->pmt_price = $value2->discount_fee;
-					$items->bn = $value2->bn;
-					$items->item_status = $value2->item_status;
-					$sku_properties = explode(':',$value2->sku_properties);
-					$pattr = new stdClass;
-					$pattr->value = $sku_properties[0];
-					$pattr->label = $sku_properties[1];
-					$items->product_attr = array(0=>$pattr);
-					$items->item_type = $value2->item_type;
-					$items->amount = $value2->total_item_fee;
-					$items->shop_goods_id = $value2->iid;//未确定字段
-					$items->amount = $value2->total_item_fee;
-					$items->sendnum = $value2->sendnum;
-					$items->sale_price = $value2->sale_price;
-					$items->quantity = $value2->num;
-					$items->price = $value2->price;
-					$items->shop_product_id = $value2->sku_id;
-					$order_items[$key] = $items;
-				}
-				$tmp->order_items = $order_items;
-				$order_objects[$key] = $tmp;
-			}
-    	}
-    	
-    	$response_data['order_objects'] = json_encode($order_objects);
-    	
-    	
+    			    	    	
     	return array('response_data'=>$response_data,'order_bn'=>$response_data['order_bn'],'from_method'=>$request_data['method'],'node_type'=>$request_data['node_type']);
     //	$CI->load->library('common/httpclient');
     	
