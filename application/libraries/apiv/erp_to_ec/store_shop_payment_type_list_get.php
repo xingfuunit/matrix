e0b962f9aaf7b4dcd6754338dddfdcf9074650d6
nick <nick@pzfresh.com>
2015-04-22 21:07:43 +0800

    支付方式同步

--- application/libraries/apiv/erp_to_ec/store_shop_payment_type_list_get.php
+++ application/libraries/apiv/erp_to_ec/store_shop_payment_type_list_get.php
@@ -49,7 +49,7 @@ class Store_shop_payment_type_list_get {
     	
     	$response_data['task'] = $request_data['task'];
     	$response_data['date'] = $request_data['date'];
-//     	$response_data['sign'] = $request_data['task'];
+//     	$response_data['sign'] = $request_data['sign'];
     	$response_data['from_api_v'] = $request_data['from_api_v'];
     	$response_data['app_id'] = $request_data['node_type'];
     	$response_data['node_id'] = $request_data['to_node_id'];
@@ -59,7 +59,7 @@ class Store_shop_payment_type_list_get {
     	return array('response_data'=>$response_data,
     			'from_method'=>$request_data['method'],
     			'node_type'=>$request_data['node_type'],
-    			'is_callback'=>true,
+    			'is_callback'=>TRUE,
     			);
     	
     }
@@ -70,37 +70,49 @@ class Store_shop_payment_type_list_get {
     function result($params){
     	$return_data = json_decode($params['return_data']);
     	
-    	return json_encode(array('res'=>'', 'msg_id'=>1, 'rsp'=>'succ', 'err_msg'=>'', 'data'=>''));
+//     	post_data_re:{"res":"succ","msg":"ok","info":""}
+    	return json_encode(array('res'=>'succ', 'msg'=>'ok', 'info'=>''));
     }
     
     
     //callback 处理
     function callback($data) {
     	$request_data = get_post(NULL);
+    	
     	$return_data = json_decode($data['return_data']);
     	$return_data = object_array($return_data);
+    	
+    	sort($return_data['data']);
     	 
-    	if ($return_data['rsp'] == 'succ') {
     		//回调接口
-    		$callback_data = array();
-    		$callback_data['res'] = '';
-    		$callback_data['err_msg'] = '';
-    		$callback_data['data'] = json_encode($return_data['data']);
-    		$callback_data['sign'] = '';
-    		$callback_data['rsp'] = 'succ';
-    		$callback_data['msg_id'] = $data['msg_id'];
-    	} else {
-    		$callback_data = array();
-    		$callback_data['res'] = $return_data['res'];
-    		$callback_data['err_msg'] = '';
-    		$callback_data['data'] = json_encode($return_data['data']);
-    		$callback_data['sign'] = '';
-    		$callback_data['rsp'] = 'fail';
-    		$callback_data['msg_id'] = $data['msg_id'];
-    	}
+    	$callback_data = array();
+    	$callback_data['res'] = '';
+    	$callback_data['err_msg'] = '';
+    	$callback_data['data'] = json_encode($this->build_data($return_data['data']));
+    	$callback_data['sign'] = '';
+    	$callback_data['rsp'] = 'succ';
+    	$callback_data['msg_id'] = $data['msg_id'];
+    	
+    	file_put_contents('api_juzhen.log', 'callback_data:'.print_r($request_data,1)."\r\n",FILE_APPEND);
     	
     	return array('callback_data'=>$callback_data,'callback_url'=>$request_data['callback_url']);
     
+    }
+    
+    /**
+     * 构造返回的数据结构
+     */
+    function build_data($return_data){
+    	
+    	$data = array();
+    	if($return_data){
+    		foreach($return_data as $k =>$v ){
+    			$data[$k]['pay_type'] 		= $v['payout_type'];
+    			$data[$k]['pay_bn'] 		= $v['payment_id'];
+    			$data[$k]['custom_name'] 	= $v['payment_name'];
+    		}
+    	}
+    	return $data;
     }
     
     
